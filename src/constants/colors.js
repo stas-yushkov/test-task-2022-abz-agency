08eb4329cf8ec26dba6b7635d3387ca053174596
stas-yushkov <stas.yushkov@gmail.com>
2022-09-20 08:34:42 +0300

    add colors

---
+++ src/constants/colors.js
@@ -0,0 +1,26 @@
+export const colors = Object.freeze({
+  PRIMARY: '#F4E041',
+  SECONDARY: '#00BDD3',
+  BACKGROUND: '#F8F8F8',
+  WHITE: '#FFFFFF',
+  // WHITE_87: 'rgba(255, 255, 255, 0.87)',
+  BLACK: '#000000',
+  BLACK_87: 'rgba(0, 0, 0, 0.87)',
+
+  buttons: {
+    primary: {
+      normal: {
+        text: 'rgba(0, 0, 0, 0.87)',
+        bg: '#F4E041'
+      },
+      hover: {
+        text: 'rgba(0, 0, 0, 0.87)',
+        bg: '#FFE302'
+      },
+      disabled: {
+        text: 'rgba(255, 255, 255, 0.87)',
+        bg: '#B4B4B4'
+      }
+    }
+  }
+});
